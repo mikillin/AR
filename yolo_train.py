ba3ddab63eb79c57cd971f706be5b934af3239ba
Sergey Rogachevsky <sergey.rogachevsky@gmail.com>
2024-10-28 21:39:31 +0100

    changed API-KEY

--- yolo_train.py
+++ yolo_train.py
@@ -2,7 +2,20 @@ from ultralytics import YOLO
 from roboflow import Roboflow
 import torch
 
-rf = Roboflow(api_key="Indm3eFaOxmcc7FrEY4D")
+
+import os
+
+# Retrieve the API key
+api_key = os.getenv("API_KEY")
+
+# Check if the API key is loaded
+if api_key:
+    print("API key loaded successfully")
+else:
+    print("API key not found")
+
+
+rf = Roboflow(api_key=api_key)
 project = rf.workspace("prj1-rfizb").project("prj1-z80sa")
 version = project.version(1)
 dataset = version.download("yolov11")
